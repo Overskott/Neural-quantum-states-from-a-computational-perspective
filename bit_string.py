4113f0718b5bfa53303523b67c3076b870a411d3
Sebastian Testanière Overksott <overskotts@gmail.com>
2022-05-11 15:24:58 +0200

    Added print and more "advanced" gaussian distribution

--- bit_string.py
+++ bit_string.py
@@ -1,21 +1,33 @@
 import random
 
 
-class BitString(int):
+class BitString(object):
 
     def __init__(self, length: int):
         self.length = length
         self.number = self.generate_perm()
 
+    def __len__(self):
+        return self.length
+
+    def __str__(self):
+        return self.get_bit_string()
+
     def get_length(self):
         return self.length
+
+    def get_bit_string(self):
+        return bin(self.number)[2:].zfill(self.length)
+
+    def get_number(self):
+        return self.number
 
     def generate_perm(self) -> int:
         """Takes a length as input, and returns a randomly generated number with binary number length = length"""
         return random.randint(0, 2 ** self.length)
 
     def flip(self):
-        binary_string = bin(self.number)[2:].zfill(self.length)  # format(self.number, 'b')
+        binary_string = self.get_bit_string()  # format(self.number, 'b')
 
         print('Number: ' + str(self.number))
         print('binary string: ' + binary_string)
@@ -32,9 +44,5 @@ class BitString(int):
             flipped = binary_string[:flip_index] + '1' + binary_string[flip_index + 1:]
 
         print('Flipped: ' + flipped)
-        print('\n')
 
         self.number = int(flipped, base=2)
-
-    def get_bit_string(self):
-        return format(self.number, 'b')
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # This is a sample Python script.
+import copy
 import random
 import numpy as np
 from bit_string import BitString
 import matplotlib.pyplot as plt
 
-def normal_distribution(x: int, sigma: float, mu: float) -> float:
+def normal_distribution(x: BitString, sigma: float, mu: float) -> float:
     """
 
     :param x:
@@ -12,50 +13,76 @@ def normal_distribution(x: int, sigma: float, mu: float) -> float:
     :param mu:
     """
     _1 = 1/(sigma*np.sqrt(2*np.pi))
-    _2 = -(1/2)*((x-mu)/sigma)**2
-    return _1*np.exp(_2)
+    _2 = -(1/2)*((x.get_number()-mu)/sigma)**2
+    _3 = _1*np.exp(_2)
+
+    _4 = 1 / (sigma/2 * np.sqrt(2 * np.pi))
+    _5 = -(1 / 2) * ((x.get_number() - mu*2) / sigma/2) ** 2
+    _6 = _4 * np.exp(_5)
+    return _3+_6
 
 
 class MCMC:
 
     def __init__(self, n, bit_length):
         self.n = n
+        self.bit_length = bit_length
         self.bit_string = BitString(bit_length)
         self.distribution = normal_distribution
-
-    def find_x_new(self):
-        self.bit_string.flip()
 
     def acceptance_criterion(self, x_new) -> bool:
         u = random.uniform(0, 1)
 
-        mu = self.bit_string.get_length()/2
+        sigma = self.bit_length
+        mu = (2**self.bit_length)/5
 
-        return self.distribution(x_new, 1, mu) / self.distribution(self.bit_string, 1, mu) > u
+        new_score = self.distribution(x_new, sigma, mu)
+        old_score = self.distribution(self.bit_string, sigma, mu)
+
+        print('u: ' + str(u))
+        print('old_score: ' + str(old_score))
+        print('New_score: ' + str(new_score))
+
+        score = new_score / old_score > u
+
+        print(score)
+        print('\n\n')
+
+        return score
 
     def metropolis(self):
         # TODO sjekke forskjell mellom reset av bit_string og ikke, mellom hver kjøring
-        x_new = self.bit_string
+        x_new = BitString(self.bit_length)
+        # x_new.number = self.bit_string.number
 
         for i in range(self.n):
-            self.find_x_new()
+            x_new.flip()
+            print('New number: ' + str(x_new.number))
 
             if self.acceptance_criterion(x_new):
-                self.bit_string = x_new
+                self.bit_string.number = x_new.number
+            else:
+                x_new.number = self.bit_string.number
 
         return x_new
 
     def average(self):
         x_hat = 0
+
+        sigma = self.bit_length
+        mu = (2 ** self.bit_length) / 5
         for i in range(self.n):
-            x_hat = x_hat + self.metropolis()
+            run = self.metropolis()
+            x_hat = x_hat + run.number
+            plt.plot(run.get_number(), normal_distribution(run, sigma, mu), 'r.')
+        plt.show()
         return x_hat/self.n
 
 
 if __name__ == '__main__':
 
-    N = 100
-    length = 7
+    N = 60
+    length = 10
 
     mcmc = MCMC(N, length)
-    mcmc.metropolis()
+    print('average: ' + str(mcmc.average()))
