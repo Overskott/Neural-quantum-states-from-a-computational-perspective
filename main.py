9b196215bb04513e223a13b366ccc10bb5396790
Sebastian T. Overskott <overskotts@gmail.com>
2022-09-26 09:32:31 +0200

    Commenting and cleaning

--- main.py
+++ main.py
@@ -5,31 +5,33 @@ from rbm import RBM
 
 if __name__ == '__main__':
 
-    walkers = 1000
-    walker_steps = 50
-    bitstring_length = 30
+    walkers = 1000  # Number of random walkers in total
+    walker_steps = 500  # Number of steps before walker termination
+    bitstring_length = 30  # Number of qubits
 
-    start_state = State(bitstring_length)
-    rbm = RBM(start_state)
+    rbm = RBM(State(bitstring_length))  # Initializing RBM currently with random configuration and parameters
 
     x_hat = 0
-    walker_list = []
-    accept_average = 0
+    walker_list = []  # list to store result of each walker
+    accept_total = 0  # Tracking the acceptance rate for each walker
 
     for i in range(walkers):
-        state = State(bitstring_length)
+        state = State(bitstring_length)  # Random starting state
         met = Metropolis(walker_steps, state, rbm.probability)
 
         run, accept_rate = met.metropolis()
         x_hat = x_hat + run.get_value()
-        accept_average += accept_rate
+        accept_total += accept_rate
         walker_list.append(run.get_value())
 
+    # Plotting histogram with results
     plt.hist(walker_list)
-    print(f"Accept rate: {accept_average / walkers}")
-    print('E: ' + str(x_hat / walkers))
-
-    plt.legend(["Target", "MCMC"], loc="upper right")
     plt.show()
+
+    # Printing results
+    print(f"Accept rate: {accept_total / walkers}")
+    print(f"Probability: {x_hat / walkers}")
+
+
 
 
--- rbm.py
+++ rbm.py
@@ -1,27 +1,34 @@
 import random
 import numpy as np
 
+
 class RBM(object):
 
-    def __init__(self, state):
+    def __init__(self, state, state_bias=None, hidden=None, hidden_bias=None, weights=None):
+
         self.s = state
         self.n = len(self.s)
-        self.h = np.asarray([random.randint(0, 1) for _ in range(self.n)])  # Hidden layer state
-        self.b = np.random.rand(self.n)  # Visible layer bias
-        self.c = np.random.rand(self.n)  # Hidden layer bias
-        self.W = np.random.rand(self.n, self.n)  # s - h weights
-        self.z = self.n  # normalization TODO Ask about this!
+        if state_bias is None:
+            self.b = np.random.rand(self.n)  # Visible layer bias
 
+        if hidden is None:
+            self.h = np.asarray([random.randint(0, 1) for _ in range(self.n)])  # Hidden layer state
+
+        if hidden_bias is None:
+            self.c = np.random.rand(self.n)  # Hidden layer bias
+
+        if weights is None:
+            self.W = np.random.rand(self.n, self.n)  # s - h weights
+        self.z = self.n  # normalization TODO Ask about this!
 
     def energy(self, state):
         '''Calculates the RBM energy'''
 
         e = 0
         e += np.transpose(self.h) @ self.W @ state
-        e += np.transpose(self.c) @ self.h
+        e += np.transpose(self.c) @ self.h  # TODO Here we need the hidden layers to calculate energy?
         e += np.transpose(self.b) @ state
         return e
-
 
     def probability(self, state):
         ''' Calculates the probability of finding the RBM in state s '''  #  TODO is it the probability of findig system is state s?
