6632bf6b418046df361ca2cb8a99ba34c347aadc
Sebastian Testanière Overskott <overskotts@gmail.com>
2023-01-29 08:48:34 +0100

    Added support for notebooks, and added more parameters in json

--- config_parser.py
+++ config_parser.py
@@ -6,8 +6,8 @@ module_path = os.path.abspath(os.path.dirname(__file__))
 source = 'config.json'
 
 if module_path not in sys.path:
-    sys.path.append(f"{module_path}//{source}")
+    sys.path.append(f"{module_path}\\{source}")
 
 def get_config_file():
-    with open('config.json') as config_file:
+    with open(f"{module_path}\\{source}") as config_file:
         return json.load(config_file)
